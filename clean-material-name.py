9053ac058615bd8da2fc239189a044470aad180d
François Klingler <francois@klingler.fr>
2023-11-16 14:23:07 +0100

    doc: move comment of .py files to docstring

--- clean-material-name.py
+++ clean-material-name.py
@@ -1,7 +1,9 @@
-import bpy
+"""
+Sets the first material name of a .blend file to a static name
+This allows us to find this material easily to append that blend file to the scene we want to render
+"""
 
-# Sets the first material name of a .blend file to a static name
-# This allows us to find this material easily to append that blend file to the scene we want to render
+import bpy
 
 material = bpy.data.materials[0];
 
--- clean-object-name.py
+++ clean-object-name.py
@@ -1,7 +1,9 @@
-import bpy
+"""
+Sets the first object name of a .blend file to a static name
+This allows us to find this object easily to append that blend file to the scene we want to render
+"""
 
-# Sets the first object name of a .blend file to a static name
-# This allows us to find this object easily to append that blend file to the scene we want to render
+import bpy
 
 object = next(filter(lambda obj: obj.parent == None, bpy.data.scenes[0].collection.objects));
 
