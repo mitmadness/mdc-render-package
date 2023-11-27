72a0f3faf423b85c2a4d782a4d885aa54286612a
François Klingler <fklingler@methodinthemadness.eu>
2023-11-27 13:18:34 +0100

    feat: force OPTIX use for render

--- render-exterior.sh
+++ render-exterior.sh
@@ -1,1 +1,1 @@
-blender -b render-scene-exterior.blend --python render-scene-import.py -o //Result#### -x 1 -f 1
+blender -b render-scene-exterior.blend --python render-scene-import.py -o //Result#### -x 1 -f 1 -- --cycles-device OPTIX
--- render-interior.sh
+++ render-interior.sh
@@ -1,1 +1,1 @@
-blender -b render-scene-interior.blend --python render-scene-import.py -o //Result#### -x 1 -f 1
+blender -b render-scene-interior.blend --python render-scene-import.py -o //Result#### -x 1 -f 1 -- --cycles-device OPTIX
