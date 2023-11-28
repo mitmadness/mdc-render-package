cdc431e755a7cbfcb0925bc8a3ccb20b622afad9
François Klingler <fklingler@methodinthemadness.eu>
2023-11-28 18:23:29 +0100

    fix: fix args usage in scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -6,15 +6,15 @@ case $1 in
         blender -b render-scene-exterior.blend --python render-scene-import.py -o //Result#### -x 1 -f 1 -- --cycles-device OPTIX
         ;;
     "prepare-material-hq-file")
-        blender -b $args[1] --python prepare-material-hq-file.py
+        blender -b $2 --python prepare-material-hq-file.py
         ;;
     "prepare-material-lq-file")
-        blender -b empty.blend --python prepare-material-lq-file.py -- $args[1]
+        blender -b empty.blend --python prepare-material-lq-file.py -- $2
         ;;
     "prepare-object-hq-file")
-        blender -b $args[1] --python prepare-object-hq-file.py
+        blender -b $2 --python prepare-object-hq-file.py
         ;;
     "prepare-object-lq-file")
-        blender -b empty.blend --python prepare-object-lq-file.py -- $args[1]
+        blender -b empty.blend --python prepare-object-lq-file.py -- $2
         ;;
 esac
