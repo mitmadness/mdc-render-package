dd5af7e26d0291e8c6ab6c8018bfce4d9befaade
François Klingler <fklingler@methodinthemadness.eu>
2023-12-19 15:31:22 +0100

    Revert "fix(mesh): recompute normals to account for the negative scale"
    This reverts commit d96a90510743a2b9d2275540cf2def94cbd400b4.
    
    This commit made some glasses have really weird reflections

--- prepare-object-hq-file.py
+++ prepare-object-hq-file.py
@@ -4,7 +4,6 @@ This allows us to find this object easily to append that blend file to the scene
 """
 
 import bpy
-import bmesh
 
 # Join all meshes
 meshes = list(filter(lambda obj: obj.type == 'MESH', bpy.data.objects))
@@ -19,15 +18,6 @@ joinedMesh = next(filter(lambda obj: obj.type == 'MESH', bpy.data.objects))
 with bpy.context.temp_override(active_object=joinedMesh, selected_objects=[joinedMesh]):
     bpy.ops.object.parent_clear(type='CLEAR_KEEP_TRANSFORM')
     bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
-
-# Recompute the normals to account for the negative scale
-bm = bmesh.new()
-bm.from_mesh(joinedMesh.data)
-bmesh.ops.recalc_face_normals(bm, faces=bm.faces)
-bm.to_mesh(joinedMesh.data)
-bm.clear()
-joinedMesh.data.update()
-bm.free()
 
 # Delete all non-meshes objects
 nonMeshes = list(filter(lambda obj: obj.type != 'MESH', bpy.data.objects))
