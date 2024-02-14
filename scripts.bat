f70c825aae9389840f9537757c80f41b21a679ce
Stéphane Becker <sbecker@home-plan.fr>
2024-02-14 13:34:59 +0100

    feat( nvidia ) : passage du script windows à OPTIX à la place de HIP

--- scripts.bat
+++ scripts.bat
@@ -1,16 +1,16 @@
 @echo off
 
 if %1 == render-interior (
-    blender -b render-scene-interior.blend --python render-scene-import.py -o //Result#### -x 1 -f 1 -- --cycles-device HIP --scene-environment interior --session %2
+    blender -b render-scene-interior.blend --python render-scene-import.py -o //Result#### -x 1 -f 1 -- --cycles-device OPTIX --scene-environment interior --session %2
 )
 if %1 == render-exterior (
-    blender -b render-scene-exterior.blend --python render-scene-import.py -o //Result#### -x 1 -f 1 -- --cycles-device HIP --scene-environment exterior --session %2
+    blender -b render-scene-exterior.blend --python render-scene-import.py -o //Result#### -x 1 -f 1 -- --cycles-device OPTIX --scene-environment exterior --session %2
 )
 if %1 == render-fast-interior (
-    blender -b cache\scene-interior-%2.blend --python render-scene-fast.py -o //Result#### -x 1 -f 1 -- --cycles-device HIP --scene-environment interior --session %2 --position %3 --orientation %4 --camera %5
+    blender -b cache\scene-interior-%2.blend --python render-scene-fast.py -o //Result#### -x 1 -f 1 -- --cycles-device OPTIX --scene-environment interior --session %2 --position %3 --orientation %4 --camera %5
 )
 if %1 == render-fast-exterior (
-    blender -b cache\scene-exterior-%2.blend --python render-scene-fast.py -o //Result#### -x 1 -f 1 -- --cycles-device HIP --scene-environment exterior --session %2 --position %3 --orientation %4 --camera %5
+    blender -b cache\scene-exterior-%2.blend --python render-scene-fast.py -o //Result#### -x 1 -f 1 -- --cycles-device OPTIX --scene-environment exterior --session %2 --position %3 --orientation %4 --camera %5
 )
 if %1 == prepare-material-hq-file (
     blender -b %2 --python prepare-material-hq-file.py
